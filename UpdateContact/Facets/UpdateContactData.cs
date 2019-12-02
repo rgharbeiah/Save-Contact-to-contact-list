a463fd2d3113ab8b584fd04b71787dc7a8e24286
Raed Abu Gharbeiah <r_aed@hotmail.com>
2019-12-02 12:00:25 +0200

    initial code

---
+++ UpdateContact/Facets/UpdateContactData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UpdateContact.Facets
+{
+    /// <summary>
+    /// UpdateContactData
+    /// </summary>
+    public class UpdateContactData
+    {
+        /// <summary>
+        /// FirstName
+        /// </summary>
+
+        public Guid FirstNameFieldId { get; set; }
+
+        /// <summary>
+        /// LastName
+        /// </summary>
+
+        public Guid LastNameFieldId { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+
+        public Guid EmailFieldId { get; set; }
+    }
+}
